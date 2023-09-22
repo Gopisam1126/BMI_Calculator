22c5a3c554d9a6bd7bd7d097480e734aaf48ad42
Gopisam1126 <gopisambhu1126@gmail.com>
2023-09-22 20:12:25 +0530

    final commit

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 function calcBmi() {
 
+    var unit = document.getElementById("unit");
     var res = document.getElementById("resBMI");
-    const age = parseInt(document.getElementById("aTbox").value);
-    const h = parseInt(document.getElementById("heightIn").value);
-    const hm = h / 100;
-    const w = parseInt(document.getElementById("weightIn").value);
-
-    const bmi = Math.round(w / (hm * hm));
-    res.textContent = bmi;
-    res.style.color = "red";
+    const age = document.getElementById("aTbox").value;
+    const agei = parseInt(age);
+    const h = document.getElementById("heightIn").value;
+    const w = document.getElementById("weightIn").value;
+    const hi = parseInt(h);
+    const hm = hi / 100;
+    const wi = parseInt(w);
 
     if (h.trim() !== "" && w.trim() !== "" && age.trim() !== "") {
-        // if (bmi < 16) {
-        //     res.style.color = "red";
-        // } else if (bmi <= 17 && bmi >= 16) {
-        //     res.style.color = "orange";
-        // } else if (bmi <= 18.5 && bmi > 17) {
-        //     res.style.color = "yellow";
-        // } else if (bmi <= 25 && bmi >18.5) {
-        //     res.style.color = "green";
-        // } else if (bmi <= 30 && bmi >25) {
-        //     res.style.color = "yellow";
-        // } else if (bmi <= 35 && bmi >30) {
-        //     res.style.color = "orange";
-        // } else if (bmi <= 40 && bmi >35) {
-        //     res.style.color = "red";
-        // } else if (bmi > 40) {
-        //     res.style.color = "#A52A2A";
-        // }
+        var bmi = Math.round(wi / (hm * hm));
+        res.textContent = bmi;
+        if (bmi < 16) {
+            res.style.color = "red";
+        } else if (bmi <= 17 && bmi >= 16) {
+            res.style.color = "orange";
+        } else if (bmi <= 18.5 && bmi > 17) {
+            res.style.color = "yellow";
+        } else if (bmi <= 25 && bmi >18.5) {
+            res.style.color = "#00bf72";
+        } else if (bmi <= 30 && bmi >25) {
+            res.style.color = "yellow";
+        } else if (bmi <= 35 && bmi >30) {
+            res.style.color = "orange";
+        } else if (bmi <= 40 && bmi >35) {
+            res.style.color = "red";
+        } else if (bmi > 40) {
+            res.style.color = "#A52A2A";
+        } else {
+            res.style.color = "#fff";
+        }
     } else {
-        res.textContent = "Please Enter The details"
+        res.textContent = "Please Enter The details";
+        res.style.color = "red";
+        res.style.marginLeft = "-8.5rem"
+        unit.textContent = "";
+
     }
+}
+
+function clearAll() {
+    res.textContent = 20;
+    res.style.color = "#00bf72";
 }
